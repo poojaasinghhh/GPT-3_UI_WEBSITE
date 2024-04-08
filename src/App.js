886897c1e2f563caf2a6e1fac650f531ef9cfe7a
Pooja Singh <poojaasinghhh22@gmail.com>
2024-04-08 21:38:18 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./container/header/Header";
+import WhatGPT from "./container/whatGpt/WhatGPT";
+import Features from "./container/features/Features";
+import Possibility from "./container/possibility/Possibility";
+import Blog from "./container/blog/Blog";
+import Footer from "./container/footer/Footer";
+import CTA from "./components/cta/CTA";
+import Navbar from "./components/navbar/Navbar";
+import Brand from "./components/brand/Brand";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>{" "}
+      <Brand />
+      <WhatGPT />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+// import { createRoot } from "react-dom/client"; // Remove this line since createRoot is not used
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root")); // Render App directly using ReactDOM.render
